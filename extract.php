66d16b8c12907cfe44d48e5f0861cb3772af3a1a
Moonstone <moonstone@cscafe.xyz>
2021-08-11 18:46:44 +0300

    Somewhat English translation…

--- extract.php
+++ extract.php
@@ -8,7 +8,7 @@ if($_GET['file'] && $_GET['imgfile']){
 	$base_file = $base_dir.$getfile;
 	dir_check($getfile);
 } else {
-	echo "No information.";
+	echo "No information. ";
 	die(header("Location: ./"));
 }
 
